685003ca2073e51b504c63f38b8ecf6bee9236fd
Rishab <rishabnegi0027@gmail.com>
2021-06-12 00:04:49 +0530

    optimized

--- Array/src/TwoSum.java
+++ Array/src/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
@@ -11,14 +13,16 @@ public class TwoSum {
 	public int[] twoSum(int[] nums, int target) {
 
 		int result[] = new int[2];
+		
+		Map<Integer,Integer> map=new HashMap<>(nums.length);
 		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				if (nums[i] + nums[j] == target) {
-					result[0] = i;
-					result[1] = j;
-					break;
-				}
+	
+			if(map.get(target-nums[i])!=null) {
+				
+				result[0]=i;
+				result[1]=map.get(target-nums[i]);
 			}
+			map.put(nums[i],i);
 		}
 		return result;
 	}
